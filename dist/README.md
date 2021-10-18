d25af7567de77d39aebae53b64369b7c26afb7a0
Lesly <leslyr562@gmail.com>
2021-10-17 19:41:46 -0500

    wrotefile

--- dist/README.md
+++ dist/README.md
@@ -1,44 +1,38 @@
 
-
-function readMeTemplate(data) {
-    
-    return `
     [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
-    # ${data.title}
-    https://github.com/${data.username}/${data.title}
+    # les
+    https://github.com/dfs/les
 
     ## Description 
-    ${data.description} 
+    lesl 
 
     ## Table of Content
 
     ## Installations
     The following is neccssary to run this application
-    ${data.installations}
+    dffdsfd
 
     ## Usage
-    In order to use this app, ${data.usage} .
+    In order to use this app, dfs .
 
     ## License
     This project is licensed under   license.
     
     ## Contributing
     This app runs under the following guidelines:
-    ${data.contributing}
+    df
 
     ##Restrictions
     This app has the following restrictions: 
-    ${data.restrictions}
+    undefined
  
     ## Test
     The following is needed to run the test:
-    ${data.test}
+    dfs
     
     ## Questions
     If you have any questions about the repo, open an issue or contact:
-    Github: ${data.username}
-    Email: ${data.email}
+    Github: dfs
+    Email: sdf
     
-    `;
-};
-module.exports = readMeTemplate
+    
--- index.js
+++ index.js
@@ -1,38 +1,83 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const makePage = require('./Template')
+const readMeTemplate = require('./Template');
 
 const promptInfo = () => {
     return inquirer.prompt([
         {
             type: 'input',
             name: 'title',
-            message: 'What is the title of the project?'
+            message: 'What is the title of the project? (Required)',
+            validate: titleInfo => {
+                if (titleInfo) {
+                    return true;
+                } else {
+                    console.log('Please enter a title!')
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'description',
-            message: 'What is the project about? Give a brief description .'
+            message: 'What is the project about? Give a brief description .',
+            validate: descriptionInfo => {
+                if (descriptionInfo) {
+                    return true;
+                } else {
+                    console.log('Please enter a description!')
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
-            name: 'instalations',
-            message: 'What does the user need to install to succesfully run this app? List any links you have used or websites that helped you in this project'
+            name: 'installations',
+            message: 'What does the user need to install to succesfully run this app? List any links you have used or websites that helped you in this project',
+            validate: installInfo => {
+                if (installInfo) {
+                    return true;
+                } else {
+                    console.log('Please enter installations or links used!')
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'usage',
-            message: 'How is the app used? Give instructions.'
+            message: 'How is the app used? Give instructions.',
+            validate: usageInfo => {
+                if (usageInfo) {
+                    return true;
+                } else {
+                    console.log('Please enter instructions!')
+                    return false;
+                }
+            }
         },
         {
-            type: 'input',
+            type: 'checkbox',
             name: 'license',
-            message: 'What license is being used?'
+            message: 'What license is being used?',
+            choices: ['Apache license 2.0', 'MIT licence', 'BSD 3-Clause "New" or "Revised" license', 'BSD 2-Clause "Simplified" or "FreeBSD" license', 'GNU General Public License (GPL)', 'GNU Library or "Lesser" General Public License (LGPL)', 'Mozilla Public License 2.0', 'Common Development and Distribution License', 'Eclipse Public License version 2.0']
         },
         {
             type: 'input',
             name: 'contributing',
-            message: 'What guidelines does the app run under? Does it have any restrictions?'
+            message: 'What guidelines does the app run under?'
+        },
+        {
+            type: 'confirm',
+            name: 'confirmRes',
+            message: 'Does it have any restrictions?',
+            default: true
+        },
+        {
+            type: 'input',
+            name: 'restrictions',
+            message: 'If restrictions, please name them:',
+            when: ({ confirmRes }) => confirmRes
         },
         {
             type: 'input',
@@ -42,20 +87,48 @@ const promptInfo = () => {
         {
             type: 'input',
             name: 'username',
-            message: 'What is your github username?',
+            message: 'Enter your Github Username. (Required)',
+            validate: githubInfo => {
+                if (githubInfo) {
+                    return true;
+                } else {
+                    console.log('Please enter your github username!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'email',
-            message: 'Enter your email address.'
+            message: 'Enter your email address. (Required)',
+            validate: emailInfo => {
+                if (emailInfo) {
+                    return true
+                } else {
+                    console.log('Please enter your email address')
+                }
+
+            }
         }
 
     ])
 }
 promptInfo()
-.then(templateData => {
-    return makePage(templateData);
-})
-// .then(pageREADME => {
-//     return writeFile(pageREADME);
-// })
+    .then(templateData => {
+        return readMeTemplate(templateData);
+    })
+    .then(pageREADME => {
+        return writeFile(pageREADME);
+    })
+
+
+
+function writeFile (input) {
+    fs.writeFile('./dist/README.md', input, err => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log('Page created! Check out README.md in this directory to see it!')
+    })
+}
